0f093bca73055d9258131223bfc65d339d40790e
Ruth7277 <131394190+RuthPoplavsky@users.noreply.github.com>
2024-09-22 00:34:06 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace yes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPayment = new System.Windows.Forms.Button();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.btnWasLesson = new System.Windows.Forms.Button();
+            this.btnOpenGroup = new System.Windows.Forms.Button();
+            this.btnAddCounselor = new System.Windows.Forms.Button();
+            this.btnAddCounselorToGroup = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(521, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "טיפול בנתונים";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPayment
+            // 
+            this.btnPayment.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPayment.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPayment.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnPayment.Location = new System.Drawing.Point(443, 289);
+            this.btnPayment.Name = "btnPayment";
+            this.btnPayment.Size = new System.Drawing.Size(149, 88);
+            this.btnPayment.TabIndex = 3;
+            this.btnPayment.Text = "תשלום";
+            this.btnPayment.UseVisualStyleBackColor = false;
+            this.btnPayment.Click += new System.EventHandler(this.btnPayment_Click);
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSignUp.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignUp.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSignUp.Location = new System.Drawing.Point(598, 101);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(147, 88);
+            this.btnSignUp.TabIndex = 4;
+            this.btnSignUp.Text = "רישום תלמיד";
+            this.btnSignUp.UseVisualStyleBackColor = false;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // btnWasLesson
+            // 
+            this.btnWasLesson.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnWasLesson.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWasLesson.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnWasLesson.Location = new System.Drawing.Point(443, 195);
+            this.btnWasLesson.Name = "btnWasLesson";
+            this.btnWasLesson.Size = new System.Drawing.Size(149, 88);
+            this.btnWasLesson.TabIndex = 5;
+            this.btnWasLesson.Text = "התקיים שעור";
+            this.btnWasLesson.UseVisualStyleBackColor = false;
+            this.btnWasLesson.Click += new System.EventHandler(this.btnWasLesson_Click);
+            // 
+            // btnOpenGroup
+            // 
+            this.btnOpenGroup.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOpenGroup.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenGroup.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnOpenGroup.Location = new System.Drawing.Point(598, 195);
+            this.btnOpenGroup.Name = "btnOpenGroup";
+            this.btnOpenGroup.Size = new System.Drawing.Size(148, 88);
+            this.btnOpenGroup.TabIndex = 6;
+            this.btnOpenGroup.Text = "פתיחת קבוצה";
+            this.btnOpenGroup.UseVisualStyleBackColor = false;
+            this.btnOpenGroup.Click += new System.EventHandler(this.btnOpenGroup_Click);
+            // 
+            // btnAddCounselor
+            // 
+            this.btnAddCounselor.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAddCounselor.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCounselor.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnAddCounselor.Location = new System.Drawing.Point(598, 289);
+            this.btnAddCounselor.Name = "btnAddCounselor";
+            this.btnAddCounselor.Size = new System.Drawing.Size(149, 88);
+            this.btnAddCounselor.TabIndex = 7;
+            this.btnAddCounselor.Text = "רישום מדריכה";
+            this.btnAddCounselor.UseVisualStyleBackColor = false;
+            this.btnAddCounselor.Click += new System.EventHandler(this.btnAddCounselor_Click);
+            // 
+            // btnAddCounselorToGroup
+            // 
+            this.btnAddCounselorToGroup.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAddCounselorToGroup.Font = new System.Drawing.Font("Ravie", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCounselorToGroup.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnAddCounselorToGroup.Location = new System.Drawing.Point(443, 101);
+            this.btnAddCounselorToGroup.Name = "btnAddCounselorToGroup";
+            this.btnAddCounselorToGroup.Size = new System.Drawing.Size(149, 88);
+            this.btnAddCounselorToGroup.TabIndex = 8;
+            this.btnAddCounselorToGroup.Text = "שבוץ מדריכה בקבוצה";
+            this.btnAddCounselorToGroup.UseVisualStyleBackColor = false;
+            this.btnAddCounselorToGroup.Click += new System.EventHandler(this.btnAddCounselorToGroup_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(26, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 491);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(916, 538);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnAddCounselorToGroup);
+            this.Controls.Add(this.btnAddCounselor);
+            this.Controls.Add(this.btnOpenGroup);
+            this.Controls.Add(this.btnWasLesson);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.btnPayment);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "yes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPayment;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.Button btnWasLesson;
+        private System.Windows.Forms.Button btnOpenGroup;
+        private System.Windows.Forms.Button btnAddCounselor;
+        private System.Windows.Forms.Button btnAddCounselorToGroup;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using yes.GUI;
+using yes.GUI.COUNSELORS;
+using yes.GUI.COUNSLERSINGROUP;
+using yes.GUI.DATESFORGROUP;
+using yes.GUI.GROUPS;
+using yes.GUI.SURFERS;
+
+namespace yes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmMainTipulBenetunim m = new frmMainTipulBenetunim();
+            this.Hide();
+            m.ShowDialog();
+            this.Show();
+
+        }
+
+       
+
+        private void btnPayment_Click(object sender, EventArgs e)
+        {
+            frmMainSurfers h = new frmMainSurfers("תשלום");
+            this.Hide();
+            h.ShowDialog();
+            this.Show();
+           
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            DialogResult ans = MessageBox.Show("האם הגולש כבר קיים?", "בדיקת גולש", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button3, MessageBoxOptions.RtlReading);
+            if (ans == DialogResult.Yes)
+            {
+                frmMainSurfers mr = new frmMainSurfers("רישום");
+                this.Hide();
+                mr.ShowDialog();
+                this.Show();
+
+
+            }
+            else
+                if (ans == DialogResult.No)
+                {
+                    frmAddUpdateSurfers AC = new frmAddUpdateSurfers("רישום");
+                    this.Hide();
+                    AC.ShowDialog();
+                    this.Show();
+                }
+            
+        }
+
+        private void btnAddCounselor_Click(object sender, EventArgs e)
+        {
+            frmAddUpdateCounselors auc = new frmAddUpdateCounselors();
+            this.Hide();
+            auc.ShowDialog();
+            this.Show();
+        }
+
+        private void btnOpenGroup_Click(object sender, EventArgs e)
+        {
+            frmAddUpdateGroups r = new frmAddUpdateGroups();
+            this.Hide();
+            r.ShowDialog();
+            this.Show();
+        }
+
+        private void btnWasLesson_Click(object sender, EventArgs e)
+        {
+            frmMainDatesForGroup r = new frmMainDatesForGroup();
+            this.Hide();
+            r.ShowDialog();
+            this.Show();
+        }
+
+        private void btnAddCounselorToGroup_Click(object sender, EventArgs e)
+        {
+            frmAddUpdateCounselorsInGroup r = new frmAddUpdateCounselorsInGroup();
+            this.Hide();
+            r.ShowDialog();
+            this.Show();
+        }
+
+     
+
+        
+
+      
+
+        
+    }
+}
